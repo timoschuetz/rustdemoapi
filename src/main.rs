ce5e4c3335b19c980b215a8466d4c5e906ba30d9
Timo Schütz <mail@tschuetz.de>
2020-08-26 08:49:49 +0200

    initial content

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+extern crate sys_info;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-async fn health(req: HttpRequest) -> impl Responder {
+async fn check() -> impl Responder {
+    let hostname = sys_info::hostname();
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+    format!("{:?} - {:?}", VERSION, hostname)
+}
+
+async fn health() -> impl Responder {
     format!("Ok")
 }
 #[actix_rt::main]
@@ -13,6 +20,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            .route("/hostname", web::get().to(check))
             .route("/health", web::get().to(health))
             .route("/{name}", web::get().to(greet))
     })
